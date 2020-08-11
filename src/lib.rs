0dc42e9d076023a06009fae07a411468d21e002a
Filipe <fc1943s@gmail.com>
2020-08-11 10:16:07 -0300

    initial boilerplate

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+// #[macro_use]
+// extern crate serde_derive;
+//
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    let stuff = vec![5, 5, 2, 0, 8];
+
+    for num in stuff {
+        let val = document.create_element("div")?;
+        val.set_inner_html(&num.to_string());
+        body.append_child(&val)?;
+    }
+
+    Ok(())
+}
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
---
+++ www/index.js
@@ -0,0 +1,2 @@
+import { run } from "rust-userscripts";
+// import { memory } from "rust-userscripts/rust_userscripts_bg";
