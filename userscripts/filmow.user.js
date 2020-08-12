07fe08daff090065781042b2a6579ef318304d4f
Filipe <fc1943s@gmail.com>
2020-08-11 23:03:37 -0300

    fixes

--- userscripts/filmow.user.js
+++ userscripts/filmow.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         userscripts-filmow
-// @version      1597196824991
+// @version      1597197705348
 
 // @match        *://filmow.com/*
 
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
-// @require      ../www/dist/bootstrap.js?v=1597196824991
+// @require      ../www/dist/bootstrap.js?v=1597197705348
 
 // @grant        none
 // ==/UserScript==
